3674e457e78cae65de8251120c127d1752213f3e
raehan <raehan900809@gmail.com>
2022-01-01 23:49:06 +0900

    add: basic add evnet

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 const $MenuForm = document.querySelector('#menuForm');
-const $MenuName = document.querySelector('#menuName')
+const $MenuName = document.querySelector('#menuName');
+
+const addMenu = e => {
+  e.preventDefault();
+
+  const newName = $MenuName.value;
+  
+  $MenuName.value = '';
+
+  console.log(newName);
+
+};
+
+$MenuForm.addEventListener('submit', addMenu);
